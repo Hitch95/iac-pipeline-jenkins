6aec8f261790efb05b587aa28abd3b1824872219
Bahloul Moufidi <bahloul.condorcet@gmail.com>
2025-04-02 14:47:56 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_credentials_validation = true
+  endpoints {
+    ec2 = "http://ip10-0-9-4-cvmih336221h46l2ts40-8080.direct.lab-boris.fr"
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
